a10767c26efb2356c4af146fda69243280088062
JessPi2t <104931453+JessPi2t@users.noreply.github.com>
2022-05-05 16:26:59 +0200

    Add management for list of country and fields to query
    Fields and countries to query can be set in a sheet, there will be added as many sheets as requested countries.

--- test.js
+++ test.js
@@ -20,6 +20,12 @@ var firstQueryNationsPlayers = `{
             name
           }
         }
+        status {
+          lastFiveSo5Appearances
+          lastFiveSo5AverageScore
+          lastFifteenSo5Appearances
+          lastFifteenSo5AverageScore
+        }
       }
       pageInfo{
         endCursor
@@ -44,6 +50,12 @@ var otherQueryNationsPlayers = `{
             name
           }
         }
+        status {
+          lastFiveSo5Appearances
+          lastFiveSo5AverageScore
+          lastFifteenSo5Appearances
+          lastFifteenSo5AverageScore
+        }
       }
       pageInfo{
         endCursor
@@ -66,6 +78,12 @@ var firstQueryNationsActivePlayers = `{
           season{
             name
           }
+        }
+        status {
+          lastFiveSo5Appearances
+          lastFiveSo5AverageScore
+          lastFifteenSo5Appearances
+          lastFifteenSo5AverageScore
         }
       }
       pageInfo{
@@ -91,6 +109,12 @@ var otherQueryNationsActivePlayers = `{
             name
           }
         }
+        status {
+          lastFiveSo5Appearances
+          lastFiveSo5AverageScore
+          lastFifteenSo5Appearances
+          lastFifteenSo5AverageScore
+        }
       }
       pageInfo{
         endCursor
@@ -112,61 +136,59 @@ function getParams(payloadContent) {
   };
 }
 
-function myFunctionTestCountries() {
+function myFunctionTestCountries_v2() {
 
   let url = "https://api.sorare.com/graphql";
 
-  var queriedNation = "france";
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("main");
+  range = sheet.getRange(1, 2);
 
-  let graphqlFirstQuery = JSON.stringify({
-    query: String.format(firstQueryNationsActivePlayers, queriedNation),
-    variables: null,
-  });
+  var queriesNations = []
+  var nationColId = 2;
+  var queriedNation = sheet.getRange(1, nationColId).getValue();
+  while (queriedNation) {
+    queriesNations.push(queriedNation)
+    queriedNation = sheet.getRange(1, ++nationColId).getValue();
+  }
 
-  var response = UrlFetchApp.fetch(url, getParams(graphqlFirstQuery));
+  var fieldsToExport = []
 
-  var lists = JSON.parse((response.getContentText()));
-  var nationalTeamData = lists.data.nationalTeam;
-  var playersList = nationalTeamData.activePlayers.nodes;
+  var fieldColId = 2;
+  var field = sheet.getRange(4, fieldColId).getValue();
+  while (field) {
+    fieldsToExport.push(field)
+    field = sheet.getRange(4, ++fieldColId).getValue();
+  }
 
-  playersList.forEach(function (item, index, array) {
+  var searchActivePlayers = sheet.getRange(2, 2).getValue() != "Y";
 
-    let cardSupply = item.cardSupply;
-    if (cardSupply.length == 0)
-      return;
+  for (var nationIdx = 0; nationIdx < queriesNations.length; nationIdx++) {
+    var queriedNation = queriesNations[nationIdx];
+    //var queriedNation = range.getValue();
+    var firstQuery = searchActivePlayers ? firstQueryNationsActivePlayers : firstQueryNationsPlayers;
+    var nextQuery = searchActivePlayers ? otherQueryNationsActivePlayers : otherQueryNationsPlayers;
 
-    let hasCard = false;
-    for (let idx = 0; idx < cardSupply.length; idx++) {
-      if (cardSupply[idx].limited > 0) {
-        hasCard = true;
-        break;
-      }
-
-    }
-    if (hasCard) {
-      console.log(item.displayName)
-      if (item.position == "Goalkeeper")
-        console.log("GK")
-
-    }
-  })
-
-  var endCursor
-  var hasNextItems = nationalTeamData.activePlayers.pageInfo.hasNextPage
-  while (hasNextItems) {
-
-    endCursor = nationalTeamData.activePlayers.pageInfo.endCursor
-
-    let graphQlSecondQuery = JSON.stringify({
-      query: String.format(otherQueryNationsActivePlayers, queriedNation, endCursor),
+    let graphqlFirstQuery = JSON.stringify({
+      query: String.format(firstQuery, queriedNation),
       variables: null,
     });
 
-    response = UrlFetchApp.fetch(url, getParams(graphQlSecondQuery));
+    var response = UrlFetchApp.fetch(url, getParams(graphqlFirstQuery));
 
-    lists = JSON.parse((response.getContentText()));
-    nationalTeamData = lists.data.nationalTeam;
-    playersList = nationalTeamData.activePlayers.nodes;
+    var lists = JSON.parse((response.getContentText()));
+    console.log("RESP: " + response.getContentText())
+    var nationalTeamData = lists.data.nationalTeam;
+    var playersData = searchActivePlayers ? nationalTeamData.activePlayers : nationalTeamData.players;
+    var playersList = playersData.nodes;
+
+    var playersWithLimitedCards = {
+      "Goalkeeper": [],
+      "Defender": [],
+      "Midfielder": [],
+      "Forward": []
+    };
+
+    var playersFoundCount = 0;
 
     playersList.forEach(function (item, index, array) {
 
@@ -182,15 +204,103 @@ function myFunctionTestCountries() {
         }
 
       }
+      
       if (hasCard) {
-        console.log(item.displayName)
-        if (item.position == "Goalkeeper")
-          console.log("GK")
+        if(!playersWithLimitedCards[item.position])
+          return;
+        playersFoundCount++;
+        playersWithLimitedCards[item.position].push(item);
       }
-
     })
 
-    hasNextItems = nationalTeamData.activePlayers.pageInfo.hasNextPage
+    var endCursor;
+    var hasNextItems = playersData.pageInfo.hasNextPage;
+    while (hasNextItems) {
 
+      endCursor = playersData.pageInfo.endCursor
+
+      let graphQlSecondQuery = JSON.stringify({
+        query: String.format(nextQuery, queriedNation, endCursor),
+        variables: null,
+      });
+
+      response = UrlFetchApp.fetch(url, getParams(graphQlSecondQuery));
+
+      lists = JSON.parse((response.getContentText()));
+      nationalTeamData = lists.data.nationalTeam;
+      playersData = searchActivePlayers ? nationalTeamData.activePlayers : nationalTeamData.players;
+      playersList = playersData.nodes;
+
+      playersList.forEach(function (item, index, array) {
+
+        let cardSupply = item.cardSupply;
+        if (cardSupply.length == 0)
+          return;
+
+        let hasCard = false;
+        for (let idx = 0; idx < cardSupply.length; idx++) {
+          if (cardSupply[idx].limited > 0) {
+            hasCard = true;
+            break;
+          }
+
+        }
+        if (hasCard) {
+          playersFoundCount++;
+          playersWithLimitedCards[item.position].push(item);
+        }
+
+      })
+
+      hasNextItems = playersData.pageInfo.hasNextPage;
+
+    }
+
+    var rowIndex = 1;
+    var colIndex = 1;
+
+    var sheetName = String.format("{0}{1}", queriedNation, searchActivePlayers ? "" : "_all");
+    var sheet = SpreadsheetApp.getActive().getSheetByName(sheetName);
+    if (!sheet) {
+      sheet = SpreadsheetApp.getActive().insertSheet(sheetName);
+      //sheet.setName(sheetName);
+    }
+
+    colIndex = 2;
+    for (let fieldsIdx = 0; fieldsIdx < fieldsToExport.length; fieldsIdx++) {
+      sheet.getRange(rowIndex, colIndex++).setValue(fieldsToExport[fieldsIdx]);
+    }
+    rowIndex += 2;
+    colIndex = 1;
+
+    for (var playersPosition in playersWithLimitedCards) {
+      var playersList = playersWithLimitedCards[playersPosition];
+      console.log(playersList)
+      var range = sheet.getRange(rowIndex, colIndex);
+      range.setValue(playersPosition);
+      colIndex = 2;
+      playersList.forEach(function (item, index, array) {
+
+        for (let fieldsIdx = 0; fieldsIdx < fieldsToExport.length; fieldsIdx++) {
+          range = sheet.getRange(rowIndex, colIndex++);
+          if (fieldsIdx == 0) {
+            range.setValue(item[fieldsToExport[fieldsIdx]]);
+          }
+          else {
+            range.setValue(item["status"][fieldsToExport[fieldsIdx]]);
+          }
+        }
+
+        rowIndex++;
+        colIndex = 2;
+      });
+      rowIndex++;
+      colIndex = 1;
+    }
+
+    sheet.autoResizeColumns(1, fieldsToExport.length + 1);
   }
+
+  //SpreadsheetApp.getActive().setActiveSheet(sheet)
+  return 0;
 }
